9421a64d94100f6ea1ceadc325080dbdd80ea0bb
To <tok12225@gmail.com>
2022-11-21 18:30:01 +0900

    mybatis 기본으로 붙였습니다.

--- mini_project/src/com/mini/run/Application.java
+++ mini_project/src/com/mini/run/Application.java
@@ -1,7 +1,10 @@
 package com.mini.run;
 
 import java.util.ArrayList;
+import static com.mini.common.Template.*;
 import java.util.Date;
+
+import org.apache.ibatis.session.SqlSession;
 
 import com.mini.dto.MemberDTO;
 import com.mini.dto.ProductDTO;
@@ -20,6 +23,26 @@ public class Application {
 		
 		OliveMemberMenu oliveMemberMenu = new OliveMemberMenu();
 		OliveCustomerMenu oliveCustomerMenu = new OliveCustomerMenu();
+		
+		
+		// Mybatis Sample code 입니다
+		// Service 객체에서 아래와 같이 사용하시면 됩니다
+		// Mapper는 개인적으로 만들어 주시고 config/mybatis-config.xml 아래의 mappes의 추가해주셔야 사용 가능합니다.
+		
+		// SelectOne 함수는 각 구현하실때 안맞으실 꺼예요
+		// 아래의 코드는 단순 DB 연결 접속을 확인하고 나의 mapper가 정상적으로 config에 올라가졌는지 확인하는 함수입니다.
+		
+		
+		
+		SqlSession sqlSession = getSqlSession();
+		java.util.Date date = sqlSession.selectOne("SampleDate");
+		
+		System.out.println(date);
+		sqlSession.close();
+		
+		
+		
+		///
 		
 		
 		/*
@@ -41,6 +64,7 @@ public class Application {
 		/* @@@@@ HS, EJ, JE  @@@@@ */
 		CustomerDTO cus = new CustomerDTO("user01", "pass01", "이영우", "F", "010-8846-4893", "1");
 		// oliveCustomerMenu.OliveMainMenu(cus);
+		
 		
 	}
 }
